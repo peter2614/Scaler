05a2e0f3fb2f2e6a16a71ad22fc84ff76c7a11ba
Peter <peterp7799@gmail.com>
2020-11-22 13:40:38 -0800

    removed unnecessary files, and included bootstrap library for bootstrap. Working on fetching the ad data.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import Row from 'react-bootstrap/Row';
+
+
+class Performance extends React.Component {
+  render() {
+    return (
+      <div>Performance Here</div>
+    )
+  }
+}
+
+class Graph extends React.Component {
+  
+}
+
+class Board extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Row style={{justifyContent: "center", paddingBottom: "20px"}}>
+          <h1>Scaler</h1>
+        </Row>
+        <Row>
+          <Table>
+            <thead>
+              <tr>
+                <th>AD_ID</th>
+                <th>Performance</th>
+                <th>Graph</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>ID</td>
+                <td><Performance /></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </Table>
+          </Row>
+      </Container>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board />
     </div>
   );
 }
 
 export default App;
+
+async function getAdInsights() {
+  try {
+    encodeURIComponent();
+    let res = await fetch('https://interview-api.sbly.com', { method: 'get' });
+    let data = await res.json();
+  }
+  catch(err) {console.log(err);}
+}
